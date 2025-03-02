c317e22998518c37eb2deace6d010fe52e0761d2
Jeremy Fuentes <jeremyfuentes201@gmail.com>
2025-03-02 14:26:53 -0600

    Guia V corregida

--- pVenta/src/database/Conexion.java
+++ pVenta/src/database/Conexion.java
@@ -1,4 +1,3 @@
-
 package database;
 
 import java.sql.Connection;
@@ -7,22 +6,37 @@ import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
 public class Conexion {
+
     private final String DB_DRIVER = "com.mysql.jdbc.Driver";
-    private final String URL="jdbc:mysql://localhost:3306/";  
+    private final String URL = "jdbc:mysql://localhost:3306/";
     private final String DB = "puntoventa";
     private final String USER = "root";
-    private final String PASSWORD = "1234"; 
-}
+    private final String PASSWORD = "1234";
 
-public  Connection conectar (){
+    public Connection connection;
+
+    public Conexion() {
+        this.connection = null;
+    }
+
+    public  Connection conectar (){
         try {
             Class .forName(DB_DRIVER);
             this.connection = DriverManager.getConnection(
                     URL + DB, USER, PASSWORD);
                    
         } catch ( ClassNotFoundException | SQLException e) {
-            JOptionPane.showMessageDialog(null, e.getMessage());
+                JOptionPane.showMessageDialog(null,"prueba desde conexion" + e.getMessage());
             System.exit(0);
         }
         return  this.connection;
     }
+    
+    public void desconnectar(){
+        try {
+            this.connection.close();
+        } catch (SQLException e) {
+             JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+    }
+}
